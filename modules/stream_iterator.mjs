4e063708dbc6f109cbc8ea2e4af245f1e0768ab8
Dmytro Kushnir <dimakush1@gmail.com>
2022-07-16 18:08:26 +0300

    add comment for iterator case

--- modules/stream_iterator.mjs
+++ modules/stream_iterator.mjs
@@ -15,10 +15,17 @@ async function range(readable) {
   }
   console.log(data);
 }
+/*
+If the loop terminates with a break, return, or a throw, the stream will be destroyed.
+In other terms, iterating over a stream will consume the stream fully.
+The stream will be read in chunks of size equal to the highWaterMark option. In the code example above, data will be in a single chunk if the file has less then 64 KiB of data because no highWaterMark option is provided to fs.createReadStream().
+*/
 
 export const print = async (filename = '') => {
   return range(createReadStream(filename)).catch(console.error);
 }
+
+
 
 /*
 # Example
